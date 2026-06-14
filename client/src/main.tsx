e59703bb4d769a319b3292f38d2e9f8baa54a079
PetrosPoll <pollakis.p6@gmail.com>
2026-06-14 04:31:02 +0300

    feat: add Manrope font support and clean up HTML links
    - Integrated @fontsource/manrope package to include font weights 400, 500, 600, and 700 in the application.
    - Removed unnecessary preconnect and stylesheet links for Google Fonts from index.html to streamline font loading.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "@fontsource/manrope/400.css";
+import "@fontsource/manrope/500.css";
+import "@fontsource/manrope/600.css";
+import "@fontsource/manrope/700.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
